0d7e5e010f907ac1eb284cf7113c0dab4f80fec7
Glen Stone <glen.stone@gmail.com>
2023-02-20 22:54:07 -0500

    Fix broken shipping price navigation

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -12,7 +12,7 @@ export class CartService {
 
   getShippingPrices() {
     return this.http.get<{ type: string; price: number }[]>(
-      '/assets/shipping.json'
+      './assets/shipping.json'
     );
   }
 
